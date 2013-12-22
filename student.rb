20b0ed74433e1df3cf3b0ad028f1ce5853ba39df
sirramongabriel <sirramongabriel@gmail.com>
2013-12-22 18:49:51 -0500

    Complete Lesson1: Seniors assignment

--- student.rb
+++ student.rb
@@ -11,6 +11,10 @@ class Student
 		grade == 12
 	end
 
+	def junior?
+		grade == 11
+	end
+
 	def to_s
 		"#{last_name}, #{first_name}"
 	end
@@ -20,11 +24,19 @@ def seniors(students)
 	students.select { |student| student.senior? }
 end
 
+def juniors(students)
+	students.select { |student| student.junior? }
+end
+
 fred = Student.new("Fred", "James", 12)
 sarah = Student.new("Sarah", "Smith", 12)
 jack = Student.new("Jack", "Gong", 11)
 all_students = [fred, sarah, jack]
 
 seniors(all_students).each do |student|
+	puts student
+end
+
+juniors(all_students).each do |student|
 	puts student
 end
